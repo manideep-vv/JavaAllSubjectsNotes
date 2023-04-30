f18375e372fdb407c3bc679a96071c2ee76d142a
ManideepKumar V V <vv.manideep@gmail.com>
2023-04-30 14:37:33 +0530

    Added docker files

--- 1.kafka/My Practice codes/SpringKafkaExcersizes/src/main/java/com/tcs/kafka/spr/SpringKafkaExcersizesApplication.java
+++ 1.kafka/My Practice codes/SpringKafkaExcersizes/src/main/java/com/tcs/kafka/spr/SpringKafkaExcersizesApplication.java
@@ -2,12 +2,14 @@ package com.tcs.kafka.spr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.query.Update;
 
 @SpringBootApplication
 public class SpringKafkaExcersizesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringKafkaExcersizesApplication.class, args);
+		Update u=new Update()
 	}
 
 }
---
+++ 5.mongodb/sample data/readme.txt
@@ -0,0 +1,2 @@
+"accounts" json colletion consits of 1716 documents
+"shipwrecks" have 11,095 documents
