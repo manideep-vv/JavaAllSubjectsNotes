3810a3497bfc400634adbf4dbf9dc55a1b184c7d
ManideepKumar V V <vv.manideep@gmail.com>
2023-09-25 07:46:12 +0530

    karate

--- 8.testing/karate/SampleRestAPI/src/test/java/bdds/ExamplesTest.java
+++ 8.testing/karate/SampleRestAPI/src/test/java/bdds/ExamplesTest.java
@@ -1,4 +1,4 @@
-package examples;
+package bdds;
 
 import com.intuit.karate.Runner;
 import com.intuit.karate.junit5.Karate;
@@ -10,13 +10,16 @@ class ExamplesTest {
     
     // this will run all *.feature files that exist in sub-directories
     // see https://github.com/intuit/karate#naming-conventions   
+//    @Karate.Test
+        public   Karate testAll() {
+        return Karate.run().relativeTo(getClass());
+    }
     @Karate.Test
-    Karate testAll() {
+    public   Karate testSpecificTags() {
         Map<String, Object> args = new HashMap();
         args.put("name", "World");
 
-        Runner.runFeature("classpath:examples/users/users.feature", args, true);
-        return Karate.run().relativeTo(getClass());
+        Runner.runFeature("classpath:bdds/empCrud.feature", args, true);
+        return Karate.run().tags("@mani").relativeTo(getClass());
     }
-    
 }
