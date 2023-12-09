14bf9a5e3f48ad78fe90d592416a318b43111c6d
ManideepKumar V V <vv.manideep@gmail.com>
2023-12-09 21:22:07 +0530

    hib notes updated

---
+++ 16.All Hibernate notes/sample queries.sql
@@ -0,0 +1,9 @@
+SELECT * FROM mani.departmend1_employees_list;
+SELECT * FROM mani.departmend1;
+SELECT * FROM mani.employeev1;
+
+
+delete from mani.departmend1 where id=12
+
+drop table mani.employeev1;
+drop table mani.departmend1;
