bc39cc9daf0056b01bc52b2f0c53a17828c0d950
ManideepKumar V V <vv.manideep@gmail.com>
2023-12-13 18:50:34 +0530

    resume

--- 16.All Hibernate notes/sample queries.sql
+++ 16.All Hibernate notes/sample queries.sql
@@ -2,8 +2,7 @@ SELECT * FROM mani.departmend1_employees_list;
 SELECT * FROM mani.departmend1;
 SELECT * FROM mani.employeev1;
 
-
-delete from mani.departmend1 where id=12
+delete from departmentd1 where id=1; 
 
 drop table mani.employeev1;
 drop table mani.departmend1;
