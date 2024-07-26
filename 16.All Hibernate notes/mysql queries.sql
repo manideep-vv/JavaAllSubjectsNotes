734b316b4db4aaa171806646aafc583ea68c4d46
ManideepKumar V V <vv.manideep@gmail.com>
2024-07-26 12:07:08 +0530

    kafka notes latest

---
+++ 16.All Hibernate notes/mysql queries.sql
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+
+
+
+
+SELECT * FROM sprbatch.batch_step_execution;
+SELECT * FROM sprbatch.batch_job_execution;
+SELECT * FROM sprbatch.batch_job_instance;
+SELECT * FROM sprbatch.batch_job_execution_params;
